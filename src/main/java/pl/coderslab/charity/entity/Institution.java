f45e9e8dcf922f9d2a4abb975a3a6e7f5a4a56ec
TazbeeR <myszkiewicz.robert81@gmail.com>
2021-11-06 01:00:43 +0100

    encja institution & repo

--- src/main/java/pl/coderslab/charity/entity/Institution.java
+++ src/main/java/pl/coderslab/charity/entity/Institution.java
@@ -13,10 +13,14 @@ import javax.persistence.Id;
 @Getter
 @Setter
 @NoArgsConstructor
-public class Category {
+
+public class Institution {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     private String name;
+
+    private String description;
 }
