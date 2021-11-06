48de4d0892b2b748b0faa89c638ddbb4e5d6fcd2
TazbeeR <myszkiewicz.robert81@gmail.com>
2021-11-06 01:23:26 +0100

    walidacja encji

--- src/main/java/pl/coderslab/charity/entity/Category.java
+++ src/main/java/pl/coderslab/charity/entity/Category.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Getter
@@ -17,6 +19,7 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @NotNull
+    @Size(min = 3, max = 50)
     private String name;
 }
--- src/main/java/pl/coderslab/charity/entity/Donation.java
+++ src/main/java/pl/coderslab/charity/entity/Donation.java
@@ -5,6 +5,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
@@ -22,22 +26,34 @@ public class Donation {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     private Integer quantity;
 
     @ManyToMany
+    @NotNull
     private List<Category> categories;
 
     @OneToOne
+    @NotNull
     private Institution institution;
 
+    @NotBlank
+    @Size(max = 100)
     private String street;
 
+    @NotBlank
+    @Size(max = 100)
     private String city;
 
+    @NotBlank
+    @Size(min = 6, max = 6)
+    @Pattern(regexp = "[0-9][0-9]-[0-9][0-9][0-9]")
     private String zipCode;
 
+    @NotNull
     private LocalDate pickUpDate;
-
+    
+    @NotNull
     private LocalTime pickUpTime;
 
     private String pickUpComment;
